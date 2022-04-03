a0ba63ae54ca05c72ebd4e44bae42c924f737574
Asuro <asuro@posteo.de>
2022-04-03 19:25:37 +0200

    amogus

---
+++ Pattern.cs
@@ -0,0 +1,180 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System.Runtime.InteropServices;
+using TPixel = SixLabors.ImageSharp.PixelFormats.Rgb24;
+
+public enum PxType : byte
+{
+    Any = 10,
+    Body = 11,
+    Face = 12,
+    NonBody = 13,
+    OptionalBody = 14,
+    VirtualBorder = 15,
+}
+
+public class Pattern
+{
+    public static Pattern[] basePatterns;
+
+    static Pattern()
+    {
+        var _ = PxType.Any;
+        var B = PxType.Body;
+        var F = PxType.Face;
+        var X = PxType.NonBody;
+        var O = PxType.OptionalBody;
+
+        basePatterns = new Pattern[] {
+            new(new PxType[,]{
+                { _, X, X, X, _ },
+                { X, B, B, B, X },
+                { O, B, F, F, _ },
+                { O, B, B, B, X },
+                { O, B, O, B, X },
+                { X, B, 0, B, X },
+                { _, X, _, X, _ },
+            }),
+            new(new PxType[,]{
+                { _, X, X, X, _ },
+                { X, B, B, B, X },
+                { O, B, F, F, _ },
+                { O, B, B, B, X },
+                { O, B, B, B, X },
+                { X, B, O, B, X },
+                { X, B, 0, B, X },
+                { _, X, _, X, _ },
+            }),
+            new(new PxType[,]{
+                { _, X, X, X, _ },
+                { X, B, B, B, X },
+                { O, B, F, F, _ },
+                { O, B, B, B, X },
+                { O, B, 0, B, X },
+                { _, X, _, X, _ },
+            }),
+        };
+    }
+
+    private Image<L8> data;
+    public int width => data.Width;
+    public int height => data.Height;
+    public PxType this[int x, int y] => (PxType)data[x, y].PackedValue;
+    public Vec2 PosFace { get; }
+    public Vec2 PosBody { get; }
+    public Dictionary<PxType, int> Error { get; } = new();
+
+    public Pattern(PxType[,] data) : this(ArrayToImage(data)) { }
+
+    public Pattern(Image<L8> data)
+    {
+        this.data = data;
+        PosBody = FindFirstPixelOffset(PxType.Body);
+        PosFace = FindFirstPixelOffset(PxType.Face);
+    }
+
+    private static Image<L8> ArrayToImage(PxType[,] data)
+    {
+        var tmpImg = new Image<L8>(data.GetLength(1), data.GetLength(0));
+        for (int y = 0; y < data.GetLength(0); ++y)
+        {
+            for (int x = 0; x < data.GetLength(1); ++x)
+            {
+                tmpImg[x, y] = new L8((byte)data[y, x]);
+            }
+        }
+        return tmpImg;
+    }
+
+    public List<Pattern> GenerateMirrorPatterns()
+    {
+        var patterns = new List<Pattern>();
+
+        foreach (var (rotation, flip) in
+            from rm in Enum.GetValues<RotateMode>()
+            from fm in Enum.GetValues<FlipMode>()
+            select (rm, fm))
+        {
+            var newData = data.Clone();
+            newData.Mutate(i => i.RotateFlip(rotation, flip));
+            patterns.Add(new Pattern(newData));
+        }
+
+        return patterns;
+    }
+
+    private Vec2 FindFirstPixelOffset(PxType type)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (this[x, y] == type)
+                {
+                    return new(x, y);
+                }
+            }
+        }
+        throw new InvalidOperationException("No face found!");
+    }
+
+    public bool Sus(int x, int y, Image<TPixel> image, MatcherDict matcher)
+    {
+        if (x + this.width >= image.Width || y + this.height >= image.Height)
+        {
+            return false;
+        }
+
+        var bodyColor = image[x + PosBody.X, y + PosBody.Y];
+        var faceColor = image[x + PosFace.X, y + PosFace.Y];
+        if (bodyColor == faceColor)
+        {
+            return false;
+        }
+
+        var matcherErrors = new Dictionary<PxType, int>();
+
+        bool FinalMatch(PxType pxType, in MatchCtx ctx)
+        {
+            if (matcher.TryGetValue(pxType, out var rule))
+            {
+                if (!rule.Matcher(in ctx))
+                {
+                    ref var error = ref CollectionsMarshal.GetValueRefOrAddDefault(matcherErrors, pxType, out _);
+                    error++;
+                    if (error > rule.MaxErr)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        const int backgroundSize = 0;
+        for (int patternY = -backgroundSize; patternY < this.height + backgroundSize; patternY++)
+        {
+            for (int patternX = -backgroundSize; patternX < this.width + backgroundSize; patternX++)
+            {
+                if (x + patternX < 0 || y + patternY < 0 || x + patternX >= image.Width || y + patternY >= image.Height)
+                {
+                    continue;
+                }
+                var color = image[x + patternX, y + patternY];
+                if (patternX < 0 || patternY < 0 || patternX >= this.width || patternY >= this.height)
+                {
+                    if(!FinalMatch(PxType.VirtualBorder, new MatchCtx(color, bodyColor, faceColor)))
+                        return false;
+
+                    continue;
+                }
+
+                if(!FinalMatch(this[patternX, patternY], new MatchCtx(color, bodyColor, faceColor)))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿global using MatcherDict = System.Collections.Generic.Dictionary<PxType, MatchRule>;
+using System;
+using System.Diagnostics;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using TPixel = SixLabors.ImageSharp.PixelFormats.Rgb24;
+
+var originalImg = Image.Load<TPixel>("src.png");
+originalImg.Mutate(i => i.Resize(originalImg.Width / 3, originalImg.Height / 3, KnownResamplers.NearestNeighbor));
+
+var targetImg = originalImg.Clone();
+targetImg.Mutate(i => i.Saturate(.08f).Contrast(.35f));
+
+var matcher = new MatcherDict();
+matcher.Add(PxType.Any, /************/ new(0, (in MatchCtx c) => true));
+matcher.Add(PxType.Body, /***********/ new(0, (in MatchCtx c) => c.pixelColor == c.bodyColor));
+matcher.Add(PxType.Face, /***********/ new(0, (in MatchCtx c) => c.pixelColor == c.faceColor));
+matcher.Add(PxType.NonBody, /********/ new(2, (in MatchCtx c) => c.pixelColor != c.bodyColor));
+matcher.Add(PxType.OptionalBody, /***/ new(0, (in MatchCtx c) => true));
+matcher.Add(PxType.VirtualBorder, /**/ new(2, (in MatchCtx c) => true));
+matcher.Add(0, /*********************/ new(0, (in MatchCtx c) => c.pixelColor != c.bodyColor));
+
+List<Pattern> patterns = Pattern.basePatterns.SelectMany(p => p.GenerateMirrorPatterns()).OrderByDescending(p => p.width * p.height).ToList();
+
+var stopwatch = Stopwatch.StartNew();
+
+var sussyBakas = new List<(Vec2, Pattern)>();
+for (int y = 0; y < originalImg.Height; ++y)
+{
+    for (int x = 0; x < originalImg.Width; ++x)
+    {
+        foreach (var pattern in patterns)
+        {
+            if (pattern.Sus(x, y, originalImg, matcher))
+            {
+                sussyBakas.Add((new(x, y), pattern));
+                break;
+            }
+        }
+    }
+}
+
+foreach (var (pos, pattern) in sussyBakas)
+{
+    var bodyColor = originalImg[pos.X + pattern.PosBody.X, pos.Y + pattern.PosBody.Y];
+    for (int patternY = 0; patternY < pattern.height; patternY++)
+    {
+        for (int patternX = 0; patternX < pattern.width; patternX++)
+        {
+            var x = patternX + pos.X;
+            var y = patternY + pos.Y;
+
+            if (pattern[patternX, patternY] is PxType.Body or PxType.Face
+                || (pattern[patternX, patternY] is PxType.OptionalBody && originalImg[x, y] == bodyColor))
+            {
+                targetImg[x, y] = originalImg[x, y];
+            }
+        }
+    }
+}
+
+Console.WriteLine($"Time: {stopwatch.Elapsed}");
+targetImg.SaveAsPng("out.png");
+
+var target3 = targetImg.Clone();
+target3.Mutate(i => i.Resize(target3.Width * 3, target3.Height * 3, KnownResamplers.NearestNeighbor));
+target3.SaveAsPng("out3.png");
+
+public record struct Vec2(int X, int Y);
+
+public record struct MatchCtx(TPixel pixelColor, TPixel bodyColor, TPixel faceColor);
+public record MatchRule(int MaxErr, MatcherFunc Matcher);
+public delegate bool MatcherFunc(in MatchCtx ctx);
